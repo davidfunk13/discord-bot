0743df7dc582f9530dc2542b99a9006787884cc2
davidfunk13 <davefunk135@gmail.com>
2019-08-13 01:56:49 -0700

    testing boit

--- build/server.js
+++ build/server.js
@@ -4,6 +4,12 @@ var _discord = _interopRequireDefault(require("discord.js"));
 
 var dotenv = _interopRequireWildcard(require("dotenv"));
 
+var Cheerio = _interopRequireWildcard(require("cheerio"));
+
+var _axios = require("axios");
+
+var _minimist = _interopRequireDefault(require("minimist"));
+
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj["default"] = obj; return newObj; } }
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
@@ -11,6 +17,11 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "d
 dotenv.config();
 var client = new _discord["default"].Client();
 var BOT_USER_TOKEN = process.env.BOT_USER_TOKEN;
+
+var searchLiveleak = function searchLiveleak(terms) {
+  console.log(terms);
+};
+
 client.on('ready', function (err) {
   if (!err) {
     console.log("Hello, ".concat(client.user.tag));
@@ -19,9 +30,20 @@ client.on('ready', function (err) {
 
   return console.log(err);
 });
-client.on('message', function (msg) {
-  if (msg.content === 'ping') {
-    msg.reply('pong.');
+client.on('message', function (message) {
+  var messageArr = message.content.split(" ");
+
+  var command = (0, _minimist["default"])(messageArr)._;
+
+  var options = (0, _minimist["default"])(messageArr);
+  console.log(command, options);
+
+  switch (command[0]) {
+    case 'salvio':
+      return message.reply('WHAT!!?!');
+
+    default:
+      return message.reply("The Amazing Salvio didnt understand you! Blessings!");
   }
 });
 client.login(BOT_USER_TOKEN).then(console.log('Login Successfull.'))["catch"](console.err);
--- server/server.js
+++ server/server.js
@@ -2,11 +2,20 @@ import Discord from 'discord.js';
 
 import * as dotenv from 'dotenv';
 
+import * as Cheerio from 'cheerio';
+
+import { axios } from 'axios';
+import parseArgs from 'minimist';
+
 dotenv.config()
 
 const client = new Discord.Client();
 
 const { BOT_USER_TOKEN } = process.env;
+
+const searchLiveleak = (terms) => {
+    console.log(terms)
+}
 
 client.on('ready', (err) => {
     if (!err) {
@@ -16,9 +25,16 @@ client.on('ready', (err) => {
     return console.log(err)
 });
 
-client.on('message', msg => {
-    if (msg.content === 'ping') {
-        msg.reply('pong.');
+client.on('message', message => {
+    const messageArr = message.content.split(" ")
+    const command = parseArgs(messageArr)._;
+    const options = parseArgs(messageArr);
+    console.log(options)
+    switch (command[0]) {
+        case 'salvio':
+            return message.reply('WHAT!!?!');
+        default:
+            return message.reply("The Amazing Salvio didnt understand you! Blessings!");
     }
 });
 
