c332be656ef4d77ea4863659ca7d42dfef6129b3
Dave <davefunk135@gmail.com>
2019-09-01 01:14:41 -0700

    experimenting with parsing arguments and improving bot base.

--- server/server.js
+++ server/server.js
@@ -1,21 +1,23 @@
-import Discord from 'discord.js';
-import Bot from './bot/bot';
-
-import * as dotenv from 'dotenv';
-
-import * as Cheerio from 'cheerio';
-
 import { axios } from 'axios';
-import parseArgs from 'minimist';
+import Bot from './bot/bot';
+import * as Cheerio from 'cheerio';
+import Discord from 'discord.js';
+import * as dotenv from 'dotenv';
 
 dotenv.config()
 
 const client = new Discord.Client();
-
 const { BOT_USER_TOKEN } = process.env;
 
+//authenticate bot with discord
+client.login(BOT_USER_TOKEN)
+    .then(console.log('Login Successfull.'))
+    .catch(console.err);
 
+//on successfull login, bot ready.
 client.on('ready', (err) => {
+    client.user.setActivity("with The Cloud People");
+
     if (!err) {
         console.log(`Hello, ${client.user.tag}`);
         return;
@@ -23,13 +25,20 @@ client.on('ready', (err) => {
     return console.log(err)
 });
 
+// message handler
 client.on('message', message => {
-    const messageArr = message.content.split(" ")
-    const command = parseArgs(messageArr)._;
-    const options = parseArgs(messageArr);
-    Bot.respond(command[0], message, options);
-});
+    const prefix = 'salvio';
+    const commandArr = message.content.split(" ");
 
-client.login(BOT_USER_TOKEN)
-    .then(console.log('Login Successfull.'))
-    .catch(console.err);
+    // Prevent The Amazing Salvio from recieving his own blessings
+    if (message.author == client.user) {
+        return
+    }
+
+    console.log(commandArr[0])
+    if (commandArr[0].toLowerCase() !== prefix || message.author.bot) return
+
+    message.reply(message.author.id);
+
+    // Bot.respond(command[0], message, options);
+});
