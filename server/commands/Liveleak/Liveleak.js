5e193c7995923bbea71421ce706e1f6accd37ec9
Dave <davefunk135@gmail.com>
2019-09-05 22:56:06 -0700

    working out best way to construct JSON with title, url, and thumbnail for each video search item with cheerio selectors

--- server/commands/Liveleak/Liveleak.js
+++ server/commands/Liveleak/Liveleak.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import Cheerio from 'cheerio';
-
+import {DIV_SELECTOR, IMG_SELECTOR, H3_SELECTOR} from './selectors';
 
 
 const Liveleak = (params, message) => {
@@ -24,21 +24,11 @@ const Liveleak = (params, message) => {
             const html = res.data;
             const $ = Cheerio.load(html);
 
-            // $('featured_items_outer')
-
-            // const titleSelector = 'body > main > section.content_main_outer > div > div > div > div.col-sm-12.col-md-8.col-xs-12.pad_none.content_inner_page_left_outer > div.col-xs-12.pad_none.featured_item_main_outer';
-
-            // $(titleSelector)
-            //     .find('a')
-            //     .each((index, element) => {
-            //         if(element.attribs.title !== undefined && element.attribs.href !== undefined) {
-            //             videos.push({title: element.attribs.title, url: element.attribs.href})
-            //         }
-            //     }).find('img').each((index, element) => {
-            //         console.log(element)
-            //     });
+        const titles = $('h3').children('a').toArray();
+        titles.map(item=> console.log({title: item.attribs.title, url:item.attribs.href}))
 
         }
+
     }).catch(err => res.json({ err: "Error occurred" }))
 }
 
---
+++ server/commands/Liveleak/Untitled-1.js
@@ -0,0 +1,180 @@
+[
+    {
+        type: 'text',
+        data: '\n',
+        parent: {
+            type: 'tag',
+            name: 'div',
+            namespace: 'http://www.w3.org/1999/xhtml',
+            attribs: [Object: null prototype],
+            'x-attribsNamespace': [Object: null prototype],
+            'x-attribsPrefix': [Object: null prototype],
+            children: [Circular],
+            parent: [Object],
+            prev: [Object],
+            next: [Object]
+        },
+        prev: null,
+        next: {
+            type: 'tag',
+            name: 'a',
+            namespace: 'http://www.w3.org/1999/xhtml',
+            attribs: [Object: null prototype],
+            'x-attribsNamespace': [Object: null prototype],
+            'x-attribsPrefix': [Object: null prototype],
+            children: [Array],
+            parent: [Object],
+            prev: [Circular],
+            next: [Object]
+        }
+    },
+    {
+        type: 'tag',
+        name: 'a',
+        namespace: 'http://www.w3.org/1999/xhtml',
+        attribs: [Object: null prototype] {
+        href: 'https://www.liveleak.com/view?t=kvBG2_1562853035'
+    },
+    'x-attribsNamespace': [Object: null prototype] { href: undefined },
+    'x-attribsPrefix': [Object: null prototype] { href: undefined },
+    children: [[Object]],
+    parent: {
+        type: 'tag',
+        name: 'div',
+        namespace: 'http://www.w3.org/1999/xhtml',
+        attribs: [Object: null prototype],
+        'x-attribsNamespace': [Object: null prototype],
+        'x-attribsPrefix': [Object: null prototype],
+        children: [Circular],
+        parent: [Object],
+        prev: [Object],
+        next: [Object]
+    },
+    prev: {
+        type: 'text',
+        data: '\n',
+        parent: [Object],
+        prev: null,
+        next: [Circular]
+    },
+    next: {
+        type: 'text',
+        data: '\n',
+        parent: [Object],
+        prev: [Circular],
+        next: [Object]
+    }
+  },
+    {
+        type: 'text',
+        data: '\n',
+        parent: {
+            type: 'tag',
+            name: 'div',
+            namespace: 'http://www.w3.org/1999/xhtml',
+            attribs: [Object: null prototype],
+            'x-attribsNamespace': [Object: null prototype],
+            'x-attribsPrefix': [Object: null prototype],
+            children: [Circular],
+            parent: [Object],
+            prev: [Object],
+            next: [Object]
+        },
+        prev: {
+            type: 'tag',
+            name: 'a',
+            namespace: 'http://www.w3.org/1999/xhtml',
+            attribs: [Object: null prototype],
+            'x-attribsNamespace': [Object: null prototype],
+            'x-attribsPrefix': [Object: null prototype],
+            children: [Array],
+            parent: [Object],
+            prev: [Object],
+            next: [Circular]
+        },
+        next: {
+            type: 'tag',
+            name: 'div',
+            namespace: 'http://www.w3.org/1999/xhtml',
+            attribs: [Object: null prototype],
+            'x-attribsNamespace': [Object: null prototype],
+            'x-attribsPrefix': [Object: null prototype],
+            children: [Array],
+            parent: [Object],
+            prev: [Circular],
+            next: [Object]
+        }
+    },
+    {
+        type: 'tag',
+        name: 'div',
+        namespace: 'http://www.w3.org/1999/xhtml',
+        attribs: [Object: null prototype] {
+        class: 'col-sm-9 col-md-9 col-xs-7 pad_none featured_items_right',
+        style: 'height: 100px; overflow:hidden;'
+    },
+    'x-attribsNamespace': [Object: null prototype] { class: undefined, style: undefined },
+    'x-attribsPrefix': [Object: null prototype] { class: undefined, style: undefined },
+    children: [
+        [Object], [Object],
+        [Object], [Object],
+        [Object], [Object],
+        [Object]
+    ],
+    parent: {
+        type: 'tag',
+        name: 'div',
+        namespace: 'http://www.w3.org/1999/xhtml',
+        attribs: [Object: null prototype],
+        'x-attribsNamespace': [Object: null prototype],
+        'x-attribsPrefix': [Object: null prototype],
+        children: [Circular],
+        parent: [Object],
+        prev: [Object],
+        next: [Object]
+    },
+    prev: {
+        type: 'text',
+        data: '\n',
+        parent: [Object],
+        prev: [Object],
+        next: [Circular]
+    },
+    next: {
+        type: 'text',
+        data: '\n',
+        parent: [Object],
+        prev: [Circular],
+        next: null
+    }
+  },
+    {
+        type: 'text',
+        data: '\n',
+        parent: {
+            type: 'tag',
+            name: 'div',
+            namespace: 'http://www.w3.org/1999/xhtml',
+            attribs: [Object: null prototype],
+            'x-attribsNamespace': [Object: null prototype],
+            'x-attribsPrefix': [Object: null prototype],
+            children: [Circular],
+            parent: [Object],
+            prev: [Object],
+            next: [Object]
+        },
+        prev: {
+            type: 'tag',
+            name: 'div',
+            namespace: 'http://www.w3.org/1999/xhtml',
+            attribs: [Object: null prototype],
+            'x-attribsNamespace': [Object: null prototype],
+            'x-attribsPrefix': [Object: null prototype],
+            children: [Array],
+            parent: [Object],
+            prev: [Object],
+            next: [Circular]
+        },
+        next: null
+    }
+]
